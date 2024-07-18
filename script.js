572ae8c6d9f7cbb597cabd45d32df0d29328001e
Vipul Kumar Chauhan <vc252@snu.edu.in>
2024-07-18 22:08:41 +0530

    add random color option

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let penColor = "black";
 let gridNumber = 10;
+let rgbSelected = false;
 
 const container = document.querySelector(".gridContainer");
 let computedStyle = getComputedStyle(container);
@@ -17,6 +18,7 @@ window.addEventListener("mouseup",()=>{
 
 const colorSelector = document.querySelector("input");
 colorSelector.addEventListener("input",()=>{
+    rgbSelected = false;
     penColor = document.querySelector("input").value;
 })
 
@@ -25,8 +27,14 @@ clearButton.addEventListener("click",()=>{
     document.querySelectorAll(".gridBox").forEach((box)=>box.style.backgroundColor = "white");
 })
 
+const rgbButton = document.querySelector("#rgb-button");
+rgbButton.addEventListener("click",()=>rgbSelected = !rgbSelected)
+
 container.addEventListener("mousemove",(e)=>{
     if (e.target.classList.contains("gridBox") && mousedown) {
+        if (rgbSelected) {
+            penColor = randomColor();
+        }
         e.target.style.backgroundColor = penColor;
     }
 })
@@ -37,6 +45,16 @@ for (let i=0; i<gridNumber*gridNumber; i++) {
     grid.style.height = `${gridSize}px`;
     grid.style.width = `${gridSize}px`;
     container.append(grid);
+}
+
+function randomColor() {
+    const hex = "0123456789ABCDEF";
+    let color = "#";
+    for (let i=0; i<6; i++) {
+        let index = Math.floor(Math.random()*16);
+        color += hex[index];
+    }
+    return color;
 }
 
 
