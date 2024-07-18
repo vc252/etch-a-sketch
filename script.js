d041febe75f3a237ee41b8bb972dab64b20e88a5
Vipul Kumar Chauhan <vc252@snu.edu.in>
2024-07-18 21:17:57 +0530

    building the basic sketch
    we have color change functionality added now

---
+++ script.js
@@ -0,0 +1,39 @@
+let penColor = "black";
+let gridNumber = 10;
+
+const container = document.querySelector(".gridContainer");
+let computedStyle = getComputedStyle(container);
+const height = parseFloat(computedStyle.height);
+let gridSize = height/gridNumber;
+let mousedown = false;
+
+window.addEventListener("mousedown",()=>{
+    mousedown = true;
+})
+
+window.addEventListener("mouseup",()=>{
+    mousedown = false;
+})
+
+const colorSelector = document.querySelector("input");
+colorSelector.addEventListener("input",()=>{
+    penColor = document.querySelector("input").value;
+})
+
+container.addEventListener("mousemove",(e)=>{
+    if (e.target.classList.contains("gridBox") && mousedown) {
+        e.target.style.backgroundColor = penColor;
+    }
+})
+
+for (let i=0; i<gridNumber*gridNumber; i++) {
+    let grid = document.createElement("div")
+    grid.classList.add("gridBox");
+    grid.style.height = `${gridSize}px`;
+    grid.style.width = `${gridSize}px`;
+    container.append(grid);
+}
+
+
+
+
